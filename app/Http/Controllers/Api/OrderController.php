2339dd9fc6fabff6d33dbd68fe0fed20b93ef097
yasminulfah <ulfahmi1719@gmail.com>
2026-02-17 03:30:10 +0700

    feat: myOrder, cancelOrder, checkout

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -36,10 +36,11 @@ class OrderController extends Controller
         $order = Order::with([
             'shipping', 
             'packaging', 
-            'orderItems.productVariant.product' 
+            'orderItems.productVariant.product',
+            'user'
         ])->find($id);
 
-        if (!$order || $order->user_id !== auth()->id()) {
+        if (!$order) {
             return response()->json([
                 'success' => false, 
                 'message' => 'Order not found'
@@ -52,9 +53,50 @@ class OrderController extends Controller
         ], 200);
     }
 
+    public function myOrders()
+    {
+        $orders = Order::where('user_id', auth()->id())
+                    ->orderBy('created_at', 'desc')
+                    ->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $orders
+        ]);
+    }
+
+    public function showMyOrder($id)
+    {
+        $order = Order::with(['orderItems.productVariant.product'])
+                    ->where('user_id', auth()->id())
+                    ->findOrFail($id);
+
+        return response()->json([
+            'success' => true,
+            'data' => $order
+        ]);
+    }
+
+    public function cancelOrder($id)
+    {
+        $order = Order::where('user_id', auth()->id())
+                    ->where('id', $id)
+                    ->where('status', 'pending') 
+                    ->firstOrFail();
+
+        $order->status = 'cancelled';
+        $order->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Pesanan berhasil dibatalkan.'
+        ]);
+    }
+
     public function checkout(Request $request): JsonResponse
     {
         $validator = Validator::make($request->all(), [ 
+            'order_id' => 'nullable|exists:orders,id',
             'customer_name' => 'nullable|string',
             'shipping_id' => 'nullable|exists:shippings,id',
             'package_id' => 'nullable|exists:packagings,id',
@@ -80,21 +122,28 @@ class OrderController extends Controller
         DB::beginTransaction();
 
         try {
+            if ($request->filled('order_id')) {
+                $order = Order::findOrFail($request->order_id);
+                foreach ($order->orderItems as $oldItem) {
+                $oldItem->productVariant->increment('stock', $oldItem->quantity);
+            }
+                $order->orderItems()->delete(); 
+            } else {
+                $order = new Order();
+                $order->order_number = 'UMA-' . strtoupper(uniqid());
+                $order->user_id = auth()->id();
+            }
             // Hitung Subtotal & Cek Stok Varian
             $subtotal = 0;
             foreach ($request->items as $item) {
-                // Ambil data varian produk
                 $variant = \App\Models\ProductVariant::find($item['product_variant_id']);
                 
-                // Cek Stok Varian
                 if ($variant->stock < $item['quantity']) {
                     throw new \Exception("Stock for variant '{$variant->name}' is insufficient.");
                 }
 
-                $itemSubtotal = $variant->price * $item['quantity'];
-                $subtotal += $itemSubtotal;
+                $subtotal += $variant->price * $item['quantity'];
 
-                // Kurangi stok varian
                 $variant->decrement('stock', $item['quantity']);
             }
 
@@ -103,34 +152,20 @@ class OrderController extends Controller
             $packagingCost = $request->package_id ? Packaging::find($request->package_id)->base_packaging_cost : 0;
             $greetingCardPrice = $request->greeting_card_price ?? 0;
             
-            // Hitung Grand Total
-            $grandTotal = $subtotal + $shippingCost + $packagingCost + $greetingCardPrice;
-            
-            // set status
-            $status = $request->source === 'offline' ? 'paid' : 'pending';
-
-            // Buat Order
-            $order = Order::create([
-                'order_number'        => 'UMA-' . strtoupper(uniqid()),
-                'user_id'             => auth()->id(),
-                'customer_name'       => $request->customer_name,
-                'shipping_id'         => $request->shipping_id,
-                'package_id'          => $request->package_id,
-                'subtotal'            => $subtotal,
-                'shipping_cost'       => $shippingCost,
-                'packaging_cost'      => $packagingCost,
-                'greeting_card_price' => $greetingCardPrice,
-                'greeting_card_note'  => $request->greeting_card_note,
-                'delivery_at'         => $request->delivery_at,
-                'shipping_address'    => $request->shipping_address,
-                'payment_method'      => $request->payment_method,
-                'grand_total'         => $grandTotal,
-                'amount_paid'         => $request->amount_paid ?? 0,
-                'amount_change'       => $request->amount_change ?? 0,
-                'status'              => $status,
-                'source'              => $request->source ?? 'online',
-            ]);
-
+            $order->customer_name = $request->customer_name;
+            $order->shipping_id = $request->shipping_id;
+            $order->package_id = $request->package_id;
+            $order->subtotal = $subtotal;
+            $order->shipping_cost = $shippingCost;
+            $order->packaging_cost = $packagingCost;
+            $order->grand_total = $subtotal + $shippingCost + $packagingCost + $greetingCardPrice;
+            $order->amount_paid = $request->amount_paid ?? 0;
+            $order->amount_change = $request->amount_change ?? 0;
+            $order->status = 'completed';
+            $order->source = 'offline';
+            $order->payment_method = $request->payment_method;
+            $order->save();
+           
             // Simpan Detail Item (order_items)
             foreach ($request->items as $item) {
                 $variant = \App\Models\ProductVariant::find($item['product_variant_id']);
@@ -148,7 +183,7 @@ class OrderController extends Controller
             return response()->json([
                 'success' => true,
                 'message' => 'Order created successfully',
-                'data'    => $order->load(['orderItems.productVariant.product']) 
+                'data'    => $order->load(['orderItems.productVariant.product', 'shipping', 'packaging']) 
             ], 201);
 
         } catch (\Exception $e) {
@@ -163,19 +198,39 @@ class OrderController extends Controller
     public function getPendingOrders(): JsonResponse
     {
         $pendingOrders = Order::where('status', 'pending')
-            ->with(['shipping', 'orderItems.productVariant.product'])
+            ->with(['shipping', 'packaging', 'orderItems.productVariant.product'])
             ->orderBy('created_at', 'asc')
             ->get();
 
+            $formattedOrders = $pendingOrders->map(function ($order) {
+                return [
+                    'id' => $order->id,
+                    'customer_name' => $order->customer_name,
+                    'shipping_id' => $order->shipping_id,
+                    'package_id' => $order->package_id,
+                    'grand_total' => $order->grand_total,
+                    'items' => $order->orderItems->map(function ($item) {
+                        return [
+                            'product_id' => $item->productVariant->product->id,
+                            'variant_id' => $item->product_variant_id,
+                            'name' => $item->productVariant->product->product_name . ' - ' . $item->productVariant->name,
+                            'price' => $item->price_at_buy,
+                            'quantity' => $item->quantity,
+                        ];
+                    }),
+                ];
+            });
+
         return response()->json([
             'success' => true,
-            'data'    => $pendingOrders
+            'data'    => $formattedOrders
         ], 200);
     }
 
     public function holdOrder(Request $request): JsonResponse
     {
         $validator = Validator::make($request->all(), [
+            'order_id' => 'nullable|exists:orders,id',
             'customer_name' => 'nullable|string',
             'items' => 'required|array',
             'items.*.variant_id' => 'required|exists:product_variants,id',
@@ -191,32 +246,36 @@ class OrderController extends Controller
 
         DB::beginTransaction();
         try {
-            $order = Order::create([
-                'order_number' => 'HOLD-' . time(),
-                'customer_name' => $request->customer_name,
-                'shipping_id' => $request->shipping_id,
-                'package_id' => $request->package_id,
-                'grand_total' => $request->grand_total,
-                'status' => 'pending',
-                'source' => 'offline',
-                'payment_method' => 'cash',
-            ]);
+            if ($request->filled('order_id')) {
+                $order = Order::findOrFail($request->order_id);
+                foreach($order->orderItems as $oldItem) {
+                    $oldItem->productVariant->increment('stock', $oldItem->quantity);
+                }
+                $order->orderItems()->delete();
+                $order->grand_total = $request->grand_total;
+            } else {
+            $order = new Order();
+                $order->order_number = 'HOLD-' . time();
+                $order->status = 'pending';
+                $order->source = 'offline';
+                $order->payment_method = 'cash';
+            }
 
-            // 2. Simpan Order Items
+            $order->customer_name = $request->customer_name;
+            $order->shipping_id = $request->shipping_id;
+            $order->package_id = $request->package_id;
+            $order->save();
+
             foreach ($request->items as $item) {
-
-                $price = $item['price'];
-                $quantity = $item['quantity'];
-                $subtotal = $price * $quantity;
+                $variant = ProductVariant::find($item['variant_id']);
 
                 $order->orderItems()->create([
                     'product_variant_id' => $item['variant_id'],
                     'quantity' => $item['quantity'],
-                    'price_at_buy' => $item['price'],
-                    'subtotal' => $subtotal,
+                    'price_at_buy' => $variant['price'],
+                    'subtotal' => $variant->price * $item['quantity'],
                 ]);
                 
-                $variant = \App\Models\ProductVariant::find($item['variant_id']);
                 $variant->decrement('stock', $item['quantity']);
             }
 
@@ -224,7 +283,7 @@ class OrderController extends Controller
             return response()->json([
                 'success' => true,
                 'message' => 'Order held successfully',
-                'data' => $order->load('orderItems.productVariant.product')
+                'data' => $order->load('orderItems.productVariant.product', 'shipping', 'packaging')
             ], 201);
 
         } catch (\Exception $e) {
